5a10fb308af8979ac13490005542ec3a3a1569f7
trishiscrazy-droid <trishcodes17@gmail.com>
2026-09-05 11:59:19 +0530

    Repetitions Solved

--- Introductory Problems/Repetitions/repetitions.cpp
+++ Introductory Problems/Repetitions/repetitions.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
-
-//goal: find length of longest substring with non repeating characters.
-int lenstrsubsw(std::string& str) {
-    if (str.length() < 2) return str.length();
-    
-    int left = 0, right = 0, res = 0;
-    bool book[4] {0};
-
-    std::map<std::strong, int> charindex = {
-        {'A', 0},
-        {'T', 1},
-        {'G', 2},
-        {'C', 4},
-    }
-    
-    while (right < str.length()) {
-        while(book[charindex.at(str[right])]) {
-            book[charindex.at(str[left])] = 0;
-        }
-    }
-}
+#include <cstdint>
+using namespace std;
 
 int main() {
+    string str;
+    cin >> str;
 
-    std::string str;
-    std::cin >> str;
-
+    uint64_t count = 1, res = 1;
+    for (uint64_t i = 1; str[i]; i++) {
+        if (str[i] == str[i - 1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+        res = max(res, count);
+    }
+    cout << to_string(res);
     return 0;
 }
