303509d24fb9d3a9f9c404aa5dc3dd811d0797b5
trishiscrazy-droid <trishcodes17@gmail.com>
2026-09-08 22:25:47 +0530

    solved number spiral

--- Introductory Problems/Number Spiral/numberspiral.cpp
+++ Introductory Problems/Number Spiral/numberspiral.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <cstdint>
 using namespace std;
 
-typedef struct Vector
-{
-public:
-    uint x, y;
-} Vector;
-
-uint goright(uint* x, i) {
-
-}
-
 int main() {
-    uint t, num = 1;
-    Vector pvec, reqvec; 
+    uint32_t t;
+    uint64_t num, y, x;
     cin >> t;
+    vector<uint64_t> res;
     while (t--) {
-        cin >> reqvec.y >> reqvec.x;
+        cin >> y >> x;
         //building the spiral
-        
-
-
+        if (max(x,y) == x) {
+            if (x & 1) {
+                num = x*x;
+                while (y-- != 1) {
+                    num--;
+                }
+            } else {
+                num = (x-1)*(x-1) + 1;
+                while (y-- != 1) {
+                    num++;
+                }
+            }
+        } else {
+            if (y & 1) {
+                num = (y-1)*(y-1) + 1;
+                while (x-- != 1) {
+                    num++;
+                }
+            } else {
+                num = y*y;
+                while (x-- != 1) {
+                    num--;
+                }
+            }
+        }
+        res.push_back(num);
     }
-
+    for(int i = 0; i < res.size(); i++) {
+        cout << to_string(res[i]) << endl;
+    }
     return 0;
 }
